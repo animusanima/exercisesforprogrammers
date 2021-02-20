5b8333134423c1b3afcd68ac8b3f05ff6fd59971
Sgro-Hinterseber <David-Sebastian.Sgro-Hinterseber@adesso-insurance-solutions.de>
2021-02-20 22:56:07 +0100

    Added PasswordCharacterTypeRandomizer

---
+++ password_generator/src/main/java/utils/PasswordCharacterTypeRandomizer.java
@@ -0,0 +1,20 @@
+package utils;
+
+import enums.PasswordCharacterType;
+
+import java.util.Random;
+
+public class PasswordCharacterTypeRandomizer {
+
+    private final static Random RANDOMIZER = new Random();
+
+    public static PasswordCharacterType getRandomCharacterType() {
+        int randomValue = RANDOMIZER.nextInt(3);
+        if (randomValue == PasswordCharacterType.SPECIAL_CHARACTER_TYPE.ordinal()) {
+            return PasswordCharacterType.SPECIAL_CHARACTER_TYPE;
+        } else if (randomValue == PasswordCharacterType.NUMBER_CHARACTER_TYPE.ordinal()) {
+            return PasswordCharacterType.NUMBER_CHARACTER_TYPE;
+        }
+        return PasswordCharacterType.NORMAL_CHARACTER_TYPE;
+    }
+}
